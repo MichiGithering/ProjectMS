48e5a745d3fc100755e22be3d04044d7b3b04a94
MichiGithering <156632341+MichiGithering@users.noreply.github.com>
2026-03-19 16:17:32 +0700

    MovementUpdate

--- Assets/Scripts/AddiationalScript/Movement.cs
+++ Assets/Scripts/AddiationalScript/Movement.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody rb;
+
+    // Public property so your "other script" can easily set this
+    [HideInInspector] public float moveInput = 0f;
+    public float moveSpeed = 5f;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        // Apply the moveInput to the Y axis for vertical movement
+        // We keep the current X velocity to avoid freezing horizontal physics
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, moveInput * moveSpeed, rb.linearVelocity.z);
     }
 }
